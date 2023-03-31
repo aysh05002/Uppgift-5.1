1b0204ae6a010fcc7a05a0dd240f6c749909a177
Admin <Admin@TEB123008>
2023-03-31 08:58:18 +0200

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        string[] sträng = new string[3];
+        sträng[0] = "Första raden";
+        sträng[1] = "Andra raden";
+        sträng[2] = "Trädje raden";
+
+        for (int i = 0; i < sträng.Length; i++)
+        {
+            Console.WriteLine(sträng[i]);
+            Console.WriteLine();
+        }
+    }
+}
